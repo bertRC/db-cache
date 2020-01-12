882e5b48ed606fa3e847b54166f6be155d7a9c85
bertRC <sithalbert@ya.ru>
2020-01-12 19:04:16 +0300

    JdbcHelper util class and additional interfaces realized

---
+++ my-app/src/main/java/education/bert/util/jdbc/JdbcHelper.java
@@ -0,0 +1,54 @@
+package education.bert.util.jdbc;
+
+import java.sql.*;
+import java.util.Optional;
+
+public class JdbcHelper {
+    private JdbcHelper() {
+    }
+
+    public static int executeUpdate(String url, String sql) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url);
+                Statement statement = connection.createStatement();
+        ) {
+            return statement.executeUpdate(sql);
+        }
+    }
+
+    public static int executeUpdate(String url, String sql, PreparedStatementSetter setter) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url);
+                PreparedStatement preparedStatement = setter.set(connection.prepareStatement(sql));
+        ) {
+            return preparedStatement.executeUpdate();
+        }
+    }
+
+    public static <T> T executeUpdateWithId(String url, String sql, PreparedStatementSetter setter) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url);
+                PreparedStatement preparedStatement = setter.set(connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS));
+        ) {
+            preparedStatement.executeUpdate();
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return (T) generatedKeys.getObject(1);
+            }
+            throw new SQLException("No keys generated");
+        }
+    }
+
+    public static <T> Optional<T> executeQueryForObject(String url, String sql, PreparedStatementSetter setter, RowMapper<T> mapper) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url);
+                PreparedStatement preparedStatement = setter.set(connection.prepareStatement(sql));
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            if (resultSet.next()) {
+                return Optional.of(mapper.map(resultSet));
+            }
+            return Optional.empty();
+        }
+    }
+}
---
+++ my-app/src/main/java/education/bert/util/jdbc/RowMapper.java
@@ -0,0 +1,9 @@
+package education.bert.util.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T map(ResultSet resultSet) throws SQLException;
+}
