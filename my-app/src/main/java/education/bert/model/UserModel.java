30c021ed607450f4be7845673910e61b4e683a6e
bertRC <sithalbert@ya.ru>
2020-01-12 23:34:46 +0300

    UserModel and PostModel modified

--- my-app/src/main/java/education/bert/model/UserModel.java
+++ my-app/src/main/java/education/bert/model/UserModel.java
@@ -1,11 +1,51 @@
 package education.bert.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
 public class UserModel {
     private int id;
     private String name;
+
+    public UserModel(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return id == userModel.id &&
+                name.equals(userModel.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
