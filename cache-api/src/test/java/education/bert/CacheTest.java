26e2b4dc1ee2e25080f59ebf050c17b593d2a88a
bertRC <sithalbert@ya.ru>
2020-01-12 12:25:12 +0300

    CacheTest realized

---
+++ cache-api/src/test/java/education/bert/CacheTest.java
@@ -0,0 +1,91 @@
+package education.bert;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CacheTest {
+    private Cache<Integer, String> cache;
+
+    @BeforeEach
+    public void setup() {
+        cache = new CacheLruImpl<>(3);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new CacheLruImpl<Integer, String>(CacheLruImpl.minimalCacheSize - 1)
+        );
+    }
+
+    @Test
+    public void shouldThrowNullPointerExceptionTest() {
+        final Cache<Integer, String> defaultCache = new CacheLruImpl<>();
+        assertThrows(NullPointerException.class, () -> defaultCache.put(null, "Some Value"));
+        assertThrows(NullPointerException.class, () -> defaultCache.put(0, null));
+        assertThrows(NullPointerException.class, () -> defaultCache.put(null, null));
+        assertThrows(NullPointerException.class, () -> defaultCache.get(null));
+        assertThrows(NullPointerException.class, () -> defaultCache.remove(null));
+    }
+
+    @Test
+    public void simplePutTest() {
+        Integer key = 0;
+        String value = "Value";
+        String previousValue = cache.put(key, value);
+        Map<Integer, String> expectedMap = new HashMap<>();
+        expectedMap.put(key, value);
+
+        assertNull(previousValue);
+        assertEquals(expectedMap, cache.getMap());
+    }
+
+    @Test
+    public void putGetTest() {
+        Integer key = 0;
+        String value = "Value";
+        cache.put(key, value);
+
+        assertEquals(value, cache.get(key));
+    }
+
+    @Test
+    public void updateValueTest() {
+        Integer key = 0;
+        String value = "Value";
+        cache.put(key, value);
+        String newValue = "New Value";
+        String previousValue = cache.put(key, newValue);
+
+        assertEquals(value, previousValue);
+        assertEquals(newValue, cache.get(key));
+    }
+
+    @Test
+    public void putWithKickOutTest() {
+        Map<Integer, String> expectedMap = new HashMap<>();
+        expectedMap.put(1, "Value1");
+        expectedMap.put(2, "Value2");
+        expectedMap.put(3, "Value3");
+        cache.put(0, "Value0");
+        cache.put(1, "Value1");
+        cache.put(2, "Value2");
+        cache.put(3, "Value3");
+
+        assertNull(cache.get(0));
+        assertEquals(expectedMap, cache.getMap());
+    }
+
+    @Test
+    public void removeTest() {
+        cache.put(0, "Value");
+        cache.remove(0);
+
+        assertNull(cache.get(0));
+    }
+}
