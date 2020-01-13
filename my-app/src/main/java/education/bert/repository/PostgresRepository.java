d40ad08ff4fcdf68b34608e05f34b175395eea76
bertRC <sithalbert@ya.ru>
2020-01-13 17:44:39 +0300

    SqlRunnable and SqlCallable interfaces applied

--- my-app/src/main/java/education/bert/repository/PostgresRepository.java
+++ my-app/src/main/java/education/bert/repository/PostgresRepository.java
@@ -4,6 +4,8 @@ import education.bert.exception.DataAccessException;
 import education.bert.model.PostModel;
 import education.bert.model.UserModel;
 import education.bert.util.jdbc.JdbcHelper;
+import education.bert.util.jdbc.SqlCallable;
+import education.bert.util.jdbc.SqlRunnable;
 
 import java.sql.SQLException;
 import java.util.Optional;
@@ -15,19 +17,35 @@ public class PostgresRepository {
         this.url = url;
     }
 
-    public void setup() {
+    private void sqlRun(SqlRunnable runnable) {
         try {
-            JdbcHelper.executeUpdate(url, "DROP TABLE IF EXISTS users;");
-            JdbcHelper.executeUpdate(url, "DROP TABLE IF EXISTS posts;");
-            JdbcHelper.executeUpdate(url, "CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT NOT NULL);");
-            JdbcHelper.executeUpdate(url, "CREATE TABLE posts (id SERIAL PRIMARY KEY, postName TEXT NOT NULL, creatorId INTEGER);");
+            runnable.run();
         } catch (SQLException e) {
             throw new DataAccessException(e);
         }
     }
 
-    public UserModel saveUser(UserModel user) {
+    private <V> V sqlCall(SqlCallable<V> callable) {
         try {
+            return callable.call();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public void setup() {
+        sqlRun(() ->
+        {
+            JdbcHelper.executeUpdate(url, "DROP TABLE IF EXISTS users;");
+            JdbcHelper.executeUpdate(url, "DROP TABLE IF EXISTS posts;");
+            JdbcHelper.executeUpdate(url, "CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT NOT NULL);");
+            JdbcHelper.executeUpdate(url, "CREATE TABLE posts (id SERIAL PRIMARY KEY, postName TEXT NOT NULL, creatorId INTEGER);");
+        });
+    }
+
+    public UserModel saveUser(UserModel user) {
+        return sqlCall(() ->
+        {
             UserModel result = new UserModel(user.getId(), user.getName());
             if (user.getId() == 0) {
                 int id = JdbcHelper.executeUpdateWithId(url, "INSERT INTO users(name) VALUES (?);", statement -> {
@@ -43,48 +61,42 @@ public class PostgresRepository {
                 });
             }
             return result;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        });
     }
 
     public Optional<UserModel> getUser(int id) {
-        try {
-            return JdbcHelper.executeQueryForObject(url, "SELECT id, name FROM users WHERE id = ?;", statement -> {
-                statement.setInt(1, id);
-                return statement;
-            }, resultSet -> new UserModel(
-                    resultSet.getInt("id"),
-                    resultSet.getString("name")
-            ));
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        return sqlCall(() ->
+                JdbcHelper.executeQueryForObject(url, "SELECT id, name FROM users WHERE id = ?;", statement -> {
+                    statement.setInt(1, id);
+                    return statement;
+                }, resultSet -> new UserModel(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name")
+                ))
+        );
     }
 
     public void removeUser(int id) {
-        try {
-            JdbcHelper.executeUpdate(url, "DELETE FROM users WHERE id = ?;", statement -> {
-                statement.setInt(1, id);
-                return statement;
-            });
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        sqlRun(() ->
+                JdbcHelper.executeUpdate(url, "DELETE FROM users WHERE id = ?;", statement -> {
+                    statement.setInt(1, id);
+                    return statement;
+                })
+        );
     }
 
     public int getUsersCount() {
-        try {
+        return sqlCall(() ->
+        {
             Optional<Integer> result = JdbcHelper.executeQueryForObject(url, "SELECT COUNT(*) AS cnt FROM users;",
                     statement -> statement, resultSet -> resultSet.getInt("cnt"));
             return result.orElse(0);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        });
     }
 
     public PostModel savePost(PostModel post) {
-        try {
+        return sqlCall(() ->
+        {
             PostModel result = new PostModel(post.getId(), post.getPostName(), post.getCreatorId());
             if (post.getId() == 0) {
                 int id = JdbcHelper.executeUpdateWithId(url, "INSERT INTO posts(postName, creatorId) VALUES (?, ?);",
@@ -104,57 +116,49 @@ public class PostgresRepository {
                         });
             }
             return result;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        });
     }
 
     public Optional<PostModel> getPost(int id) {
-        try {
-            return JdbcHelper.executeQueryForObject(url, "SELECT id, postName, creatorId FROM posts WHERE id = ?;", statement -> {
-                statement.setInt(1, id);
-                return statement;
-            }, resultSet -> new PostModel(
-                    resultSet.getInt("id"),
-                    resultSet.getString("postName"),
-                    resultSet.getInt("creatorId")
-            ));
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        return sqlCall(() ->
+                JdbcHelper.executeQueryForObject(url, "SELECT id, postName, creatorId FROM posts WHERE id = ?;", statement -> {
+                    statement.setInt(1, id);
+                    return statement;
+                }, resultSet -> new PostModel(
+                        resultSet.getInt("id"),
+                        resultSet.getString("postName"),
+                        resultSet.getInt("creatorId")
+                ))
+        );
     }
 
     public void removePost(int id) {
-        try {
-            JdbcHelper.executeUpdate(url, "DELETE FROM posts WHERE id = ?;", statement -> {
-                statement.setInt(1, id);
-                return statement;
-            });
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        sqlRun(() ->
+                JdbcHelper.executeUpdate(url, "DELETE FROM posts WHERE id = ?;", statement -> {
+                    statement.setInt(1, id);
+                    return statement;
+                })
+        );
     }
 
     public int getPostsCount() {
-        try {
+        return sqlCall(() ->
+        {
             Optional<Integer> result = JdbcHelper.executeQueryForObject(url, "SELECT COUNT(*) AS cnt FROM posts;",
                     statement -> statement, resultSet -> resultSet.getInt("cnt"));
             return result.orElse(0);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        });
     }
 
     public int getPostsCountForCreator(int creatorId) {
-        try {
+        return sqlCall(() ->
+        {
             Optional<Integer> result = JdbcHelper.executeQueryForObject(url, "SELECT COUNT(*) AS cnt FROM posts WHERE creatorId = ?;",
                     statement -> {
                         statement.setInt(1, creatorId);
                         return statement;
                     }, resultSet -> resultSet.getInt("cnt"));
             return result.orElse(0);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        });
     }
 }
